178d19f52c2a76c540e107a5194d2a8d3ecdffcd
root <root@logs-1.c.infra-dev-249211.internal>
2020-09-07 01:16:41 +0000

    upate autoexec

--- tools/autoexec/main.go
+++ tools/autoexec/main.go
@@ -16,13 +16,32 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type stats struct {
+	Max 			  float64 `json:"max"`
+	StandardDeviation float64 `json:"standardDeviation"`
+	Mean 			  float64 `json:"mean"`
+}
+
+type jsonStruct struct {
+	Difficulty 		stats `json:"difficulty"`
+	TotalDifficulty stats `json:"totalDifficulty"`
+	GasLimit 		stats `json:"gasLimit"`
+	GasUsed 		stats `json:"gasUsed"`
+	BlockTime 		stats `json:"blockTime"`
+	BlockSize 		stats `json:"blockSize"`
+	TransactionPerBlock stats `json:"transactionPerBlock"`
+	UncleCount 		stats `json:"uncleCount"`
+	Tps 			stats `json:"tps"`
+	Blocks 			float64 `json:"blocks"`
+}
+
 type TestEnv struct {
 	HostName 	 string `json:"hostName"`
 	TestName 	 string `json:"testName"`
 	TimeBegin 	 int64	`json:"timeBegin"`
 	TimeEnd 	 int64	`json:"timeEnd"`
 	TestID		 string	`json:"testID"`
-	WebStats	 string	`json:"webStats"`
+	WebStats	 jsonStruct	`json:"webStats"`
 }
 
 type SysEnv struct {
@@ -39,7 +58,7 @@ type SysEnv struct {
 	autoExecLog	 string
 	pathYaml	 string
 	externalIP	 string
-	webStats	 string
+	webStats	 jsonStruct
 	rstatsPID	 int
 }
 
@@ -53,7 +72,7 @@ func (test *SysEnv) setDefaults() error {
 	test.pathYaml	  = "/var/log/syslog-ng/ef-testing/autoexec-yaml"
 	test.externalIP	  = ""
 /*	test.webStats	  = `{"difficulty":{"max":55000000,"standardDeviation":427516.00232242176,"mean":51202982.53106213},"totalDifficulty":{"max":25550288283,"standardDeviation":7391818514.634528,"mean":12768191452.43888},"gasLimit":{"max":11850000,"standardDeviation":595.7486135081332,"mean":11849961.018036073},"gasUsed":{"max":11371336,"standardDeviation":508257.5740591193,"mean":11342176.352705412},"blockTime":{"max":631,"standardDeviation":30.57527317048098,"mean":13.160965794768613},"blockSize":{"max":163758,"standardDeviation":7276.488035583049,"mean":162884.8316633266},"transactionPerBlock":{"max":25,"standardDeviation":1.1180317437084863,"mean":24.949899799599194},"uncleCount":{"max":1,"standardDeviation":0.09959738388608554,"mean":0.010020040080160317},"tps":{"max":25,"standardDeviation":7.716263384080033,"mean":6.520443852228358},"blocks":499}`*/
-	test.webStats	  = ""
+/*	test.webStats	  = ""*/
 	
 	nglog_file := test.pathSyslogNG+test.efLog
 	if _, err := os.Stat(nglog_file); os.IsNotExist(err) {
@@ -157,20 +176,48 @@ return nil
 
 func (test *SysEnv) monitorWebData() error {
 	statsURL := "http://"+test.webDataURL
+	startRstats := 0
 	for {
 		resp, err := http.Get(statsURL)
-		if err != nil {
-			log.WithFields(log.Fields{"endpoint": statsURL, "resp": resp,"error": err}).Error("Unable to make http request.")
-			return fmt.Errorf("Unable to make http request.")
+		if resp == nil {
+			log.WithFields(log.Fields{"endpoint": statsURL, "resp": resp, "error": err}).Info("Unable to make http request, trying again.")
+			time.Sleep(15 * time.Second)
+			continue
 		}
-
 		body, err := ioutil.ReadAll(resp.Body)
 		if string(body) != "" {
-			var result map[string]interface{}
+			if startRstats == 0 {
+				// Start RSTATS data collection
+				ok := test.startRstats()
+				if ok != nil {
+					log.WithFields(log.Fields{"error": ok}).Error("Unable to start RSTATS collection.")
+					return fmt.Errorf("Unable to start RSTATS process.")
+				}
+				
+				startRstats = 1
+			}
+
+			var result = jsonStruct{}
 			json.Unmarshal([]byte(body), &result)
-			if result["blocks"].(int) > 420 {
+			test.webStats = result
+
+			split := strings.Split(test.fileName, "/")
+			slice_file := strings.Split(split[len(split)-1], ".")
+	
+			time_now := time.Now()
+			logStats := TestEnv{}
+			logStats.HostName 	 = test.hostName
+			logStats.TestName 	 = slice_file[0]
+			logStats.TimeBegin 	 = test.timeBegin
+			logStats.TimeEnd 	 = time_now.Unix()
+			logStats.TestID		 = test.testID
+			logStats.WebStats	 = test.webStats
+
+			jsonTest, _ := json.Marshal(logStats)
+			fmt.Println(string(jsonTest))
+
+			if result.Blocks > 420 {
 				// set test.webStats to write to file when test is done
-				test.webStats = string(body)
 				resp.Body.Close()
 				break
 			}
@@ -184,35 +231,35 @@ return nil
 
 func (test *SysEnv) startRstats() error {
 	// genesis stats cad --json -t 670e1118-5620-4c91-8560-eafd14c73048  >> /var/log/syslog-ng/autoexec-log/670e1118-5620-4c91-8560-eafd14c73048.stats
-	cmd := exec.Command("genesis", "stats", "cad", "-t", test.testID, "--json", ">>", test.pathSyslogNG+test.pathLog+test.testID+".stats")
-	err := cmd.Start()
+	file_name := test.pathSyslogNG+test.pathLog+test.testID+".stats"
+	file, err := os.OpenFile(file_name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.WithFields(log.Fields{"cmd": cmd, "error": err}).Error("Unable to start RSTATS process.")
+		log.WithFields(log.Fields{"file_name": file_name, "error": err}).Error("Unable to open RSTATS file.")
 		return fmt.Errorf("Unable to start RSTATS process.")
-	} 
-	fmt.Println(cmd)
+	}
+	cmd := exec.Command("genesis", "stats", "cad", "--json", "-t", test.testID)
+	cmd.Stdout = file
+	cmd.Stderr = file
+	err = cmd.Start()
+	if err != nil {
+		log.WithFields(log.Fields{"cmd": cmd, "file_name": file_name, "error": err}).Error("Unable to start RSTATS function.")
+		return fmt.Errorf("Unable to start RSTATS function.")
+	}
 	test.rstatsPID = cmd.Process.Pid
-	
-/*
-	go func() { 
-			err := cmd.Wait()
-			log.WithFields(log.Fields{"cmd": cmd, "error": err}).Error("Unable to execute RSTATS process.")
-		}()
-*/
+
 return nil
 }
 
 func (test *SysEnv) beginTest() error {
-	cmd := exec.Command("genesis", "run", test.fileName, test.UserName, "--no-await", "--json")
+/*	cmd := exec.Command("genesis", "run", test.fileName, test.UserName, "--no-await", "--json")*/
+	cmd := exec.Command("genesis", "run", test.fileName, "paccode", "--no-await", "--json")
 	fmt.Println(cmd)
-/*
     out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.WithFields(log.Fields{"ip_address": ip, "out": string(out), "cmd": cmd, "error": err}).Error("Unable to set genesis syslogng host.")
-		return fmt.Errorf("Unable to set genesis syslogng host.")
-
-	} 
-*/
+		log.WithFields(log.Fields{"file": test.fileName, "out": string(out), "cmd": cmd, "error": err}).Error("Unable to start genesis run host.")
+		return fmt.Errorf("Unable to start genesis run host.")
+	}
+	fmt.Println(string(out))
 return nil
 }
 
@@ -328,6 +375,8 @@ func main() {
 		time_now := time.Now()
 		test.timeBegin = time_now.Unix()
 		test.fileName = file[i]
+		fmt.Println("test ", i, "Begin --- ", test)
+
     	// Begin genesis run yaml_file test
     	err = test.beginTest()
     	if err != nil {
@@ -346,20 +395,13 @@ func main() {
 			log.WithFields(log.Fields{"yaml_file": file, "error": err}).Error("Unable to get test web data URL.")
 			return 
     	}
-    	// Start RSTATS data collection
-    	test.startRstats()
-    	if err != nil {
-			log.WithFields(log.Fields{"yaml_file": file, "error": err}).Error("Unable to start RSTATS collection.")
-			return 
-    	}
-/*
     	// Start Webdata collection
+    	// this function start RSTATS collection as soon as webstats start to come in
     	err = test.monitorWebData()
     	if err != nil {
 			log.WithFields(log.Fields{"yaml_file": file, "error": err}).Error("Unable to monitor web stats.")
 			return 
     	}
-*/
 		fmt.Println(test)
     	// Test has finished cleanup and get ready for the next test
     	err = test.cleanUp(0)
